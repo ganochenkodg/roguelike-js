a98deda9e0ad22cc67ab22a8cee1de287a6cc08e
dganochenko <dganochenko@tradingview.com>
2019-12-07 13:09:59 +0300

    add extreme index page

---
+++ assets/configextreme.js
@@ -0,0 +1,8 @@
+var MapWidth = 25;
+var MapHeight = 13;
+//chance of rare monster, %
+var RareMobChance = 80;
+//chance that rare becomes a boss
+var RareBossChance = 30;
+//default chance of rare item
+var RareItemDefaultChance = 35;
